9519a86d128838e3f2722f65a1689259f2318082
William Wong <wonghinfung0325@gmail.com>
2023-05-20 18:27:13 -0400

    setup mongodb database and user can login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongo";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
