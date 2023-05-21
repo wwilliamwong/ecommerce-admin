f86ef90b74cf1ca025c846d46391b88057bef97d
William Wong <wonghinfung0325@gmail.com>
2023-05-21 07:18:32 -0400

    bug: fixed session bug

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  if (!session) return;
   return (
     <Layout>
       <div className="text-blue-900">Hello, {session?.user?.email}</div>
