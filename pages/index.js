4ff145b5c83a413cbcd708ba4607de79817b751d
William Wong <wonghinfung0325@gmail.com>
2023-05-21 07:23:34 -0400

    feat: added user icon

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Layout from "../components/Layout";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) return;
   return (
     <Layout>
-      <div className="text-blue-900">Hello, {session?.user?.email}</div>
+      <div className="text-blue-900">
+        Hello, {session?.user?.name}
+        <img src={session?.user?.image} alt="" />
+      </div>
     </Layout>
   );
 }
