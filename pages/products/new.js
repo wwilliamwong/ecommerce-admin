7af76958cec5adf97fed19cc3ebfb3c0fb2b1f9a
William Wong <wonghinfung0325@gmail.com>
2023-05-21 09:44:23 -0400

    feat: setup api for adding new product

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,17 +1,44 @@
 import Layout from "@/components/Layout";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+  }
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label htmlFor="">Product Name</label>
-      <input type="text" placeholder="Product Name" />
-      <label htmlFor="">Description</label>
-      <textarea placeholder="Description"></textarea>
-      <label htmlFor="">Price (in USD)</label>
-      <input type="" placeholder="Price" />
-      <button className="btn-primary">Save</button>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label htmlFor="">Product Name</label>
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+        <label htmlFor="">Description</label>
+        <textarea
+          placeholder="Description"
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        ></textarea>
+        <label htmlFor="">Price (in USD)</label>
+        <input
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(ev) => setPrice(ev.target.value)}
+        />
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 }
