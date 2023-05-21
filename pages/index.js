de20ce2029baef292cbea4cd45c4ec40b95db9ea
William Wong <wonghinfung0325@gmail.com>
2023-05-20 22:00:15 -0400

    added style to log in dashboard

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,11 @@ export default function Home() {
     );
   }
   return (
-    <div className="bg-blue-900 min-h-screen">
+    <div className="bg-blue-900 min-h-screen flex">
       <Nav />
-      <div> logged in {session.user.email}</div>
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        logged in {session.user.email}
+      </div>
     </div>
   );
 }
