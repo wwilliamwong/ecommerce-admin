652f1353bf24942bf460c6f83b714031da45e63a
William Wong <wonghinfung0325@gmail.com>
2023-05-21 08:14:26 -0400

    feat: added button in product page

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,14 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 function Products() {
-  return <Layout>products page here</Layout>;
+  return (
+    <Layout>
+      <Link className="bg-gray-300 rounded-md py-1 px-1" href={"/products/new"}>
+        Add new product
+      </Link>
+    </Layout>
+  );
 }
 
 export default Products;
